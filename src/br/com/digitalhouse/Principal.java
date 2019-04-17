4463b855e141df23006d3e0b822991773a25427f
sergioitazar <itazar@gmail.com>
2019-04-17 12:08:59 -0300

    teste commit 3

--- src/br/com/digitalhouse/Principal.java
+++ src/br/com/digitalhouse/Principal.java
@@ -4,11 +4,15 @@ public class Principal{
     public static void main(String[] args) {
 
         // construir aluno alimentar a classe aluno - criar objeto
-        Aluno tairo = new Aluno("1234A","Tairo","Roberto");
-        Aluno jessica = new Aluno("2345B","Jessica","Correa");
+        Aluno joao = new Aluno("1234A","Joao","Roberto");
+        Aluno maria = new Aluno("2345B","Maria","Correa");
         Aluno sergio = new Aluno ("3456C","Sergio","Souza");
+        Aluno chico = new Aluno ("2222H","Chico","Xavier");
 
-
+// construir professor - alimentar a classe professor que vira objeto
+        Professor tario = new Professor ("Tairo","44A","Android","2");
+        Professor jessica = new Professor("Jessica","55J","Java","3");
+        
 
     }
 }
