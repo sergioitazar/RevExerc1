c6264587e0aa0176c39d83ec1f4ef570fcf86dc6
sergioitazar <itazar@gmail.com>
2019-04-17 11:42:24 -0300

    teste commit 2

--- src/br/com/digitalhouse/Principal.java
+++ src/br/com/digitalhouse/Principal.java
@@ -1,6 +1,14 @@
 package br.com.digitalhouse;
 
-public class Principal {
+public class Principal{
     public static void main(String[] args) {
 
+        // construir aluno alimentar a classe aluno - criar objeto
+        Aluno tairo = new Aluno("1234A","Tairo","Roberto");
+        Aluno jessica = new Aluno("2345B","Jessica","Correa");
+        Aluno sergio = new Aluno ("3456C","Sergio","Souza");
+
+            
+
     }
+}
